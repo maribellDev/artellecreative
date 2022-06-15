b7b6463c1159e22b463581365d3031da6f190820
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-06-15 22:27:04 +0300

    fix: delete from App

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,6 @@
 import "../styles/globals.scss";
-import { hotjar } from "react-hotjar";
-import { useEffect } from "react";
-import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    hotjar.initialize(2961492, 6);
-  }, []);
-  <Script>
-    <Script
-      strategy="lazyOnload"
-      src={`https://www.googletagmanager.com/gtag/js?id=G-S7QWMT71TD`}
-    />
-    <Script strategy="lazyOnload">
-      {`window.dataLayer = window.dataLayer || [];
-function gtag() {
-dataLayer.push(arguments);
-}
-gtag("js", new Date());
-
-gtag("config", "G-S7QWMT71TD");`}
-    </Script>
-  </Script>;
-
   return <Component {...pageProps} />;
 }
 
--- pages/_document.js
+++ pages/_document.js
@@ -4,14 +4,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <link rel="stylesheet" href="https://use.typekit.net/mod3ewt.css" />
-          {/*   <Script
-            async
-            type="text/javascript"
-            src={`https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=YtnfYs`}
-          ></Script> */}
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
