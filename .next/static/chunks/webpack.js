32816029b036ab3e547e5bb36ebaf2a30ec69187
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-06-18 22:15:33 +0300

    add: menu

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c2c78c02e67ad1e"; }
+/******/ 		__webpack_require__.h = function() { return "277bbefa83310579"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,50 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
-
 import Logo from "../public/logo/Logo-up.svg";
 import styles from "../styles/navigation.module.scss";
 import style from "../styles/home.module.scss";
 import { Navadapt } from "../Components/Navadapt";
+import Menuui from "./Menuui";
 
 export function Navbar() {
   return (
-    <nav className={styles.navbar}>
-      <div>
-        <Link href={"/"}>
-          <a>
-            <span className={styles.logo1}>
-              <Image src={Logo} alt="Logo" />
-            </span>
-          </a>
-        </Link>
-      </div>
+    <div className={styles.navbar}>
+      <Menuui />
       <Navadapt />
-      <div></div>
-      <div className={styles.btnMenu}>
-        <span className={styles.line1}></span>
-        <span className={styles.line2}></span>
-        <span className={styles.line3}></span>
-      </div>
-
-      <div className={styles.menu}>
-        <Link href={"/work"}>
-          <a>Work</a>
-        </Link>
-
-        <Link href={"/about"}>
-          <a>About</a>
-        </Link>
-
-        <Link href={"/blog"}>
-          <a>Blog</a>
-        </Link>
-
-        <Link href={"/contact"}>
-          <a style={{ paddingRight: "0px", color: "white" }}>
-            <button className={style.dark}>Get Started</button>
-          </a>
-        </Link>
-      </div>
-    </nav>
+    </div>
   );
 }
