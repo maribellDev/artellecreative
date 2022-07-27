4205deebcf4d3773bc9706e55023535e1a4cc15e
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-07-27 13:05:04 +0300

    fix: menu

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -54,6 +54,11 @@
 /******/ 	__webpack_require__.i = [];
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/amd options */
+/******/ 	!function() {
+/******/ 		__webpack_require__.amdO = {};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/chunk loaded */
 /******/ 	!function() {
 /******/ 		var deferred = [];
@@ -128,7 +133,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9fcf12e5342bc6c3"; }
+/******/ 		__webpack_require__.h = function() { return "0c8b50b79cb91e3f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
