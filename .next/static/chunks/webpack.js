dbdda678a64fd47013384fb2bfdef477b774baa9
Maribelll <48179911+Maribelll@users.noreply.github.com>
2022-08-11 11:22:40 +0300

    add:animation to menu

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ac72c1b873a2f3af"; }
+/******/ 		__webpack_require__.h = function() { return "7570e2ccddfc45ff"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
