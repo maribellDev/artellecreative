dab02a2f4f3ad13314eba3faf795e74610682bc3
Maribelll <48179911+Maribelll@users.noreply.github.com>
2023-10-18 14:26:24 -0400

    add: works

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5f36c14922b7da04"; }
+/******/ 		__webpack_require__.h = function() { return "d7a89a23b8744e37"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
