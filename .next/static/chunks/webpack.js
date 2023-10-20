1c1730e27bc02b3adc1fab01a4b600ecfcdbf285
Maribelll <48179911+Maribelll@users.noreply.github.com>
2023-10-20 16:48:51 -0400

    add: node

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "130659f67442de55"; }
+/******/ 		__webpack_require__.h = function() { return "928715c3e43ed5b7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
