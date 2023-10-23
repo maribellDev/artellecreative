f8660f8494325848eda5e7af8983f89cf1c67fd1
Maribelll <48179911+Maribelll@users.noreply.github.com>
2023-10-23 16:37:40 -0400

    add: changes blog2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e455b6b538f12cc8"; }
+/******/ 		__webpack_require__.h = function() { return "c4dfc4e6c384d1c8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
