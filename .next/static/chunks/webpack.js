d5ddd6d459d21bdadef27b7c39fc6edbe054587c
Maribelll <48179911+Maribelll@users.noreply.github.com>
2023-11-01 14:03:57 -0400

    add: robots

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "37b1e2213597a444"; }
+/******/ 		__webpack_require__.h = function() { return "490042bbc7c136cf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
