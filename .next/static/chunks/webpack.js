b175e7cefb64c81e6c6e7fe9b4a2ec02bbc3e744
Maribelll <48179911+Maribelll@users.noreply.github.com>
2023-11-15 09:29:44 -0500

    fix: meta img

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a8442fcc4ba82404"; }
+/******/ 		__webpack_require__.h = function() { return "4efdefd6cce34895"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
