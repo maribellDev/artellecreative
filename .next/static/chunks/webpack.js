4c04638f80ab09a320bd311ebb65032c539658ef
maribellDev <maria.beliachyts@gmail.com>
2025-08-28 13:41:49 -0400

    Initial commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "96f50054d21c632b"; }
+/******/ 		__webpack_require__.h = function() { return "bbe5a66f8a877679"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
