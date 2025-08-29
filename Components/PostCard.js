e07684e86273874be92288d8c4ad1ef4283e8578
maribellDev <maria.beliachyts@gmail.com>
2025-08-28 21:36:57 -0400

    fix: delete dinamic blog post

--- Components/PostCard.js
+++ Components/PostCard.js
@@ -1,34 +1,34 @@
-import Link from "next/link";
-import style from "/styles/blogpage.module.scss";
-import Image from "next/image";
+// import Link from "next/link";
+// import style from "/styles/blogpage.module.scss";
+// import Image from "next/image";
 
-export default function PostCard({ blogPage }) {
-  const { title, slug, onClick, thumbnail } = blogPage.fields;
+// export default function PostCard({ blogPage }) {
+//   const { title, slug, onClick, thumbnail } = blogPage.fields;
 
-  return (
-    <div>
-      <div className={style.post}>
-        <div className={style.blogImage}>
+//   return (
+//     <div>
+//       <div className={style.post}>
+//         <div className={style.blogImage}>
           
-          <img
-            src={"https:" + thumbnail?.fields?.file?.url}
+//           <img
+//             src={"https:" + thumbnail?.fields?.file?.url}
      
-            alt="strategy"
-            id={style.imagesize}
-          ></img>
-        </div>
+//             alt="strategy"
+//             id={style.imagesize}
+//           ></img>
+//         </div>
 
-        <p className={style.postname}>{title}</p>
+//         <p className={style.postname}>{title}</p>
 
-        <Link href={`/posts/${slug}`}>
-          <a>
-            <span className={style.more} onClick={onClick}>
-              Read more
-            </span>
-          </a>
-        </Link>
-      </div>
-    </div>
-  );
-}
+//         <Link href={`/posts/${slug}`}>
+//           <a>
+//             <span className={style.more} onClick={onClick}>
+//               Read more
+//             </span>
+//           </a>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// }
 
